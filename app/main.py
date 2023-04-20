e817bf742f4cc3afdfd25ad9a0d2e87d7b90803c
Jan Sowa <jan.piotr.sowa@gmail.com>
2023-04-20 09:49:07 +0200

    Change in API from url parameter to request body

--- app/main.py
+++ app/main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
 from models.heuristic import Heuristic
+from pydantic import BaseModel
+from enum import Enum
+
+class ModelTypeEnum(str, Enum):
+    heuristic = "heuristic"
+    logistic_regression = "logistic_regression"
+    random_forest = "random_forest"
+    neural_network = "neural_network"
+
+class PredictRequest(BaseModel):
+    values: list[float]
+    type: ModelTypeEnum
 
 app = FastAPI()
 
-@app.get("/model")
-async def root(values: str):
-    heuristic = Heuristic()
-    values = [float(val) for val in values.split(",")]
-    prediction = heuristic.predict(values)
-    return {"prediction": prediction}
+@app.post("/model")
+async def predict(request_body: PredictRequest):
+    if request_body.type == ModelTypeEnum.heuristic:
+        model = Heuristic()
+    else:
+        return {
+            "error": "Not implemented yet"
+        }
+    return {
+        "prediction": model.predict(request_body.values)
+    }
