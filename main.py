433e636ae194142b1bc5c2a9eb9eb82ef8dfddd7
Jan Sowa <jan.piotr.sowa@gmail.com>
2023-04-20 21:12:35 +0200

    Remove __pycache__ and add it to .gitignore, add changes missing in last commit

--- main.py
+++ main.py
@@ -2,24 +2,10 @@ from fastapi import FastAPI
 from app.models.heuristic import Heuristic
 from app.models.randomforest import RandomForest
 from app.models.logisticregression import LogisticRegression
-from pydantic import BaseModel, Field
-from enum import Enum
 import numpy as np
 
-
-class ModelTypeEnum(str, Enum):
-    heuristic = "heuristic"
-    logistic_regression = "logistic_regression"
-    random_forest = "random_forest"
-    neural_network = "neural_network"
-
-
-class PredictRequest(BaseModel):
-    values: list[float] = Field(title="Values for single prediction", min_items=54, max_items=54)
-    type: ModelTypeEnum = Field(title="Model type",
-                                description="Possible values - \"heuristic\", "
-                                            "\"logistic_regression\", \"random_forest\", \"neural_network\"")
-
+from app.dto.predictrequest import PredictRequest
+from tools.modeltypeenum import ModelTypeEnum
 
 app = FastAPI()
 
