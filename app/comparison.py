03fcb40357ad22ec7137f481be18191a30dd264e
Jan Sowa <jan.piotr.sowa@gmail.com>
2023-04-21 13:11:24 +0200

    Add neural network to comparison script

--- app/comparison.py
+++ app/comparison.py
@@ -1,26 +1,45 @@
-from app.models.generating_scripts.untrained_model import untrained_logistic_regression
-from app.models.generating_scripts.untrained_model import untrained_random_forest
+from app.models.generating_scripts.untrained_model import untrained_logistic_regression, untrained_random_forest, untrained_neural_network
 from app.models.heuristic import Heuristic
 from app.tools.evaluation import print_roc_curves, plot_models_accuracy, predict_proba_to_class
 from app.tools.load_data import LoadData
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+
+HEURISTIC = "heuristic"
+RANDOM_FOREST = "random forest"
+LOGISTIC_REGRESSION = "logistic regression"
+NEURAL_NETWORK = "neural network"
 
 X_train, X_test, y_train, y_test = LoadData().load_X_y_splitted()
-# X_train, X_test, y_train, y_test = X_train.iloc[:2000, :], X_test.iloc[:1000, :], y_train[:2000], y_test[:1000]
+X_train, X_test, y_train, y_test = X_train.iloc[:2000, :], X_test.iloc[:1000, :], y_train[:2000], y_test[:1000]
 
 accuracies = dict()
 y_score = dict()
 
 logistic_regression = untrained_logistic_regression()
-y_score["logistic regression"] = logistic_regression.fit(X_train, y_train).predict_proba(X_test)
-accuracies["logistic regression"] = accuracy_score(y_test, predict_proba_to_class(y_score["logistic regression"]))
+y_score[LOGISTIC_REGRESSION] = logistic_regression.fit(X_train, y_train).predict_proba(X_test)
+accuracies[LOGISTIC_REGRESSION] = accuracy_score(y_test, predict_proba_to_class(y_score[LOGISTIC_REGRESSION]))
 
 random_forest = untrained_random_forest()
-y_score["random forest"] = random_forest.fit(X_train, y_train).predict_proba(X_test)
-accuracies["random forest"] = accuracy_score(y_test, predict_proba_to_class(y_score["random forest"]))
+y_score[RANDOM_FOREST] = random_forest.fit(X_train, y_train).predict_proba(X_test)
+accuracies[RANDOM_FOREST] = accuracy_score(y_test, predict_proba_to_class(y_score[RANDOM_FOREST]))
 
-y_score["heuristic"] = accuracy_score(y_test, Heuristic().predict(X_test))
+
+scaler = MinMaxScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.fit_transform(X_test)
+y_train_dummies = pd.get_dummies(y_train)
+y_test_dummies = pd.get_dummies(y_test)
+neural_network = untrained_neural_network()
+neural_network.fit(X_train_scaled, y_train_dummies, epochs=10) # TODO: set more epochs
+y_score[NEURAL_NETWORK] = neural_network.predict(X_test_scaled)
+accuracies[NEURAL_NETWORK] = accuracy_score(y_test, predict_proba_to_class(y_score[NEURAL_NETWORK]))
+
+
+y_score[HEURISTIC] = accuracy_score(y_test, Heuristic().predict(X_test))
 
 plot_models_accuracy(accuracies)
-print_roc_curves(y_score["logistic regression"], y_train, y_test, "logistic regression")
-print_roc_curves(y_score["random forest"], y_train, y_test, "random forest")
+
+for model in [LOGISTIC_REGRESSION, RANDOM_FOREST, NEURAL_NETWORK]:
+    print_roc_curves(y_score[model], y_train, y_test, model)
--- app/models/generating_scripts/untrained_model.py
+++ app/models/generating_scripts/untrained_model.py
@@ -4,7 +4,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import make_pipeline
 from sklearn.ensemble import RandomForestClassifier
-
+import pandas as pd
+import tensorflow as tf
+from app.definitions import ROOT_DIR
 
 def untrained_logistic_regression():
     k_best = 35
@@ -21,3 +23,25 @@ def untrained_random_forest():
     selector = SelectKBest(score_func=mutual_info_classif, k=k_best)
     clf = RandomForestClassifier(random_state=0, n_jobs=-1, n_estimators=n_estimators, max_depth=30)
     return make_pipeline(selector, clf)
+
+def untrained_neural_network():
+    params_df = pd.read_csv(ROOT_DIR + "/models/saved/best_nn_params.csv")
+    params = pd.Series(params_df.iloc[:, 1].values, index=params_df.iloc[:, 0].values)
+
+    activation_function = params["activation_function"]
+    if activation_function == "leaky_relu":
+        activation_function = tf.keras.layers.LeakyReLU()
+
+    first_dense_units = 2 ** int(params["first_dense_power"])
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(first_dense_units, activation=activation_function),
+        tf.keras.layers.Dense(first_dense_units * 2, activation=activation_function),
+        tf.keras.layers.Dense(first_dense_units * 4, activation=activation_function),
+        tf.keras.layers.Dense(7, activation='softmax')
+    ])
+    sgd = tf.keras.optimizers.legacy.SGD(learning_rate=float(params["learning_rate"]), momentum=float(params["momentum"]))
+
+    model.compile(optimizer=sgd,
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
