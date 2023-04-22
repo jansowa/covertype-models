beba01e6f5fa9ed3e9ccb4405ef56504dea7ce9f
Jan Sowa <jan.piotr.sowa@gmail.com>
2023-04-22 13:00:42 +0200

    Extract method for getting model by name and move it to enum file

--- app/dto/model_type_enum.py
+++ app/dto/model_type_enum.py
@@ -1,8 +1,27 @@
 from enum import Enum
 
+from app.models.abstract_model import AbstractModel
+from app.models.heuristic import Heuristic
+from app.models.logistic_regression import LogisticRegression
+from app.models.neural_network import NeuralNetwork
+from app.models.random_forest import RandomForest
+
 
 class ModelTypeEnum(str, Enum):
     heuristic = "heuristic"
     logistic_regression = "logistic_regression"
     random_forest = "random_forest"
     neural_network = "neural_network"
+
+
+def get_model_type(model_type: ModelTypeEnum) -> AbstractModel:
+    if model_type == ModelTypeEnum.heuristic:
+        return Heuristic()
+    elif model_type == ModelTypeEnum.random_forest:
+        return RandomForest()
+    elif model_type == ModelTypeEnum.logistic_regression:
+        return LogisticRegression()
+    elif model_type == ModelTypeEnum.neural_network:
+        return NeuralNetwork()
+    else:
+        raise NotImplementedError
--- main.py
+++ main.py
@@ -1,59 +1,32 @@
 from fastapi import FastAPI, UploadFile
-from app.models.heuristic import Heuristic
-from app.models.random_forest import RandomForest
-from app.models.logistic_regression import LogisticRegression
-from app.models.neural_network import NeuralNetwork
-from app.models.abstract_model import AbstractModel
 import numpy as np
 import pandas as pd
 from io import BytesIO
 
 from app.dto.predict_request import PredictRequest
-from app.dto.model_type_enum import ModelTypeEnum
+from app.dto.model_type_enum import ModelTypeEnum, get_model_type
 
 app = FastAPI()
 
 
 @app.post("/model", description="Method for single sample prediction")
-async def predict(request_body: PredictRequest):
-    model: AbstractModel
-    if request_body.type == ModelTypeEnum.heuristic:
-        model = Heuristic()
-    elif request_body.type == ModelTypeEnum.random_forest:
-        model = RandomForest()
-    elif request_body.type == ModelTypeEnum.logistic_regression:
-        model = LogisticRegression()
-    elif request_body.type == ModelTypeEnum.neural_network:
-        model = NeuralNetwork()
-    else:
-        return {
-            "error": "Not implemented yet"
-        }
+async def predict_single_sample(request_body: PredictRequest):
+    model_type: ModelTypeEnum = request_body.type
+    model = get_model_type(model_type)
     return {
         "prediction": np.array(model.predict(np.array(request_body.values).reshape(1, -1))).tolist()
     }
 
+
 @app.post("/model/file", description="Load csv.file and return predictions for all samples")
-async def predict_file(csv_file: UploadFile, model_type: ModelTypeEnum):
+async def predict_from_file(csv_file: UploadFile, model_type: ModelTypeEnum):
     contents = csv_file.file.read()
     data = BytesIO(contents)
     df = pd.read_csv(data, header=None)
     data.close()
     csv_file.file.close()
+    model = get_model_type(model_type)
 
-    model: AbstractModel
-    if model_type == ModelTypeEnum.heuristic:
-        model = Heuristic()
-    elif model_type == ModelTypeEnum.random_forest:
-        model = RandomForest()
-    elif model_type == ModelTypeEnum.logistic_regression:
-        model = LogisticRegression()
-    elif model_type == ModelTypeEnum.neural_network:
-        model = NeuralNetwork()
-    else:
-        return {
-            "error": "Not implemented yet"
-        }
     return {
         "predictions": np.array(model.predict(df.values[:, :54])).tolist()
     }
