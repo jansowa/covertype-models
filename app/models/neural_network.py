f8f3811b5ca099fcc07e148eaf68084c11391f52
Jan Sowa <jan.piotr.sowa@gmail.com>
2023-04-22 07:36:20 +0200

    Add MinMaxScaler to class with trained neural network

--- app/models/neural_network.py
+++ app/models/neural_network.py
@@ -1,6 +1,7 @@
 from app.tools.file_connector import load_neural_network
 from app.models.abstract_model import AbstractModel
 from app.tools.evaluation import predict_proba_to_class
+from sklearn.preprocessing import MinMaxScaler
 
 
 class NeuralNetwork(AbstractModel):
@@ -9,4 +10,6 @@ class NeuralNetwork(AbstractModel):
         self._classifier = load_neural_network()
 
     def predict(self, X):
-        return predict_proba_to_class(self._classifier.predict(X))
+        # TODO: load this scaler from file trained on the whole dataset
+        X_scaled = MinMaxScaler().fit_transform(X)
+        return predict_proba_to_class(self._classifier.predict(X_scaled))
