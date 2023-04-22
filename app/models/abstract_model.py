8ed582e3a8489143821a974b4a2e215b1dd1da7d
Jan Sowa <jan.piotr.sowa@gmail.com>
2023-04-22 13:34:44 +0200

    Return list with proper length in AbstractModel

--- app/models/abstract_model.py
+++ app/models/abstract_model.py
@@ -2,4 +2,4 @@ class AbstractModel(object):
     _classifier = None
 
     def predict(self, X):
-        return 2 # the most frequent value
+        return [2 for _ in range(X.shape[0])]  # the most frequent value
